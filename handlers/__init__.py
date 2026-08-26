e528fa8ba9a6df2360521bccb71b5876e6154c9d
Tamim Developer <149662843+tamimowner@users.noreply.github.com>
2026-08-26 17:08:07 +0600

    Update handlers init and add railway config

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -1,10 +1,12 @@
 from aiogram import Router
 from handlers.start import router as start_router
 from handlers.menu import router as menu_router
+from handlers.chat_member import router as chat_member_router
 
 
 def setup_routers() -> Router:
     root = Router()
     root.include_router(start_router)
     root.include_router(menu_router)
+    root.include_router(chat_member_router)
     return root
