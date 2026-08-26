6052609acc8795ac9b0ef5753c4d7c3ecd065376
Tamim Developer <149662843+tamimowner@users.noreply.github.com>
2026-08-26 22:03:42 +0600

    Export settings keyboards

--- keyboards/__init__.py
+++ keyboards/__init__.py
@@ -1,1 +1,8 @@
-from keyboards.inline import language_keyboard, main_menu, premium_keyboard, back_keyboard
+from keyboards.inline import (
+    language_keyboard,
+    main_menu,
+    premium_keyboard,
+    back_keyboard,
+    settings_keyboard,
+    settings_language_keyboard,
+)
