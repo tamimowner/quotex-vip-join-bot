2dd7e296d99923249940221e888d8bc2d13e71f4
Tamim Developer <149662843+tamimowner@users.noreply.github.com>
2026-08-26 17:52:02 +0600

    Use normalized async DATABASE_URL

--- database/db.py
+++ database/db.py
@@ -2,7 +2,7 @@ from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, Asyn
 from config import settings
 from database.models import Base
 
-engine = create_async_engine(settings.DATABASE_URL, echo=False)
+engine = create_async_engine(settings.database_url, echo=False)
 async_session = async_sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
 
 
